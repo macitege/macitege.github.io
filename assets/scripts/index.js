041a458a2972d5df0213f902aa160ed4c3dbacdc
macitege <macitegeercan@gmail.com>
2019-02-10 13:18:03 -0500

    Add About Me Section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,14 @@
-$(() => {
+'use strict'
 
+const Typed = require('typed.js')
+const $ = require('jquery')
+
+$(() => {
+  console.log('Hello, curious mind. How are you? I have been thinking about you.')
+
+  // console.log('works')
+  // const typed = new Typed('#typed', {
+  //   stringsElement: '#typed-strings',
+  //   typeSpeed: 60
+  // })
 })
