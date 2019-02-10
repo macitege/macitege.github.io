f2fbc5921b944ee5755381683e9b5bc8e1696847
macitege <macitegeercan@gmail.com>
2019-02-10 13:20:33 -0500

    Compile and push again

--- bundle.js
+++ bundle.js
@@ -5,7 +5,7 @@ const Typed = require('typed.js')
 const $ = require('jquery')
 
 $(() => {
-  console.log('Hello, curious mind. How are you?')
+  console.log('Hello, curious mind. How are you? I have been thinking about you.')
 
   // console.log('works')
   // const typed = new Typed('#typed', {
