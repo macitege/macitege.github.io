ff019720bdc6fa4295e7ceeb6a6fea989b653549
macitege <macitegeercan@gmail.com>
2019-02-17 13:36:58 -0500

    Color changes, new skills section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,6 @@ const $ = require('jquery')
 
 $(() => {
   console.log('Hello, curious mind. How are you? I have been thinking about you.')
-
   // console.log('works')
   // const typed = new Typed('#typed', {
   //   stringsElement: '#typed-strings',
