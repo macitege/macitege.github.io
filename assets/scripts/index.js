b48e8a979c2958797793371966110a42a9d7916c
macitege <macitegeercan@gmail.com>
2019-03-06 21:52:47 -0500

    Add Contact Section, Make uptop disappear

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 const Typed = require('typed.js')
 const $ = require('jquery')
 
+$('.uptop').hide()
 $(() => {
   console.log('ಠ_ಠ Hello..?')
   const typed = new Typed('#typed', {
@@ -10,5 +11,21 @@ $(() => {
     typeSpeed: 60,
     loop: true,
     backDelay: 1500
+  })
+
+  $('.uptop').on('click', () => {
+    $(window).scrollTop()
+    $('.main-content').scrollTop()
+  })
+
+  $(window).scroll(() => {
+    const depth = $(window).scrollTop()
+    depth > 800 ? $('.uptop').fadeIn() : $('.uptop').fadeOut()
+  })
+
+  $('.main-content').scroll((event) => {
+    const depthW = $(window).scrollTop()
+    const depth = $('.main-content').scrollTop()
+    depth > 800 || depthW > 1  ? $('.uptop').fadeIn() : $('.uptop').fadeOut()
   })
 })
